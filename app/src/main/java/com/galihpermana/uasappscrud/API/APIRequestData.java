292f92e4e7da9c17ca6f5f8f987759c12fdf6fc5
galihperma <galihpriv99@gmail.com>
2021-07-25 00:06:00 +0700

    first commit

---
+++ app/src/main/java/com/galihpermana/uasappscrud/API/APIRequestData.java
@@ -0,0 +1,41 @@
+package com.galihpermana.uasappscrud.API;
+
+import com.galihpermana.uasappscrud.Model.ResponseModel;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface APIRequestData {
+    @GET("retrieve.php")
+    Call<ResponseModel> ardRetrieveData();
+
+    @FormUrlEncoded
+    @POST("create.php")
+    Call<ResponseModel> ardCreateData(
+            String customer , String part_name , String part_code);
+
+    @FormUrlEncoded
+    @POST("delete.php")
+    Call<ResponseModel> ardDeleteData(
+            @Field("id") int id
+    );
+
+    @FormUrlEncoded
+    @POST("get.php")
+    Call<ResponseModel> ardGetData(
+            @Field("id") int id
+    );
+
+    @FormUrlEncoded
+    @POST("update.php")
+    Call<ResponseModel> ardUpdateData(
+            @Field("id") int id,
+            @Field("customer") String customer,
+            @Field("part_name") String part_name,
+            @Field("part_code") String part_code
+    );
+}
+
---
+++ app/src/main/java/com/galihpermana/uasappscrud/Activity/Add.java
@@ -0,0 +1,16 @@
+package com.galihpermana.uasappscrud.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.galihpermana.uasappscrud.R;
+
+public class Add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
